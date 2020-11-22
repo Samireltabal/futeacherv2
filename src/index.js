fcedc43d6278b3a903629f17ecb41bb423806367
Samir eltabal <samir.m.eltabal@gmail.com>
2020-11-22 19:56:04 +0200

    url parameters

--- src/index.js
+++ src/index.js
@@ -2,21 +2,17 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 UIkit.use(Icons);
 // components can be called from the imported UIkit reference
-UIkit.notification('اهلاً بالعالم');
+// UIkit.notification('اهلاً بالعالم');
 import $ from 'jquery'
 import './index.css';
-import { ping } from './js/app'
+import { ping, getUrlParameter } from './js/app'
 
 $(document).ready(function () {
     $('#tester').text(process.env.STREAM_HOST)
     $('#tester').append(`<br> ${process.env.WEBSOCKET_HOST}`)
     ping().then((response) => {
         $('#loading').hide();
-        $('#content').html(`
-            <pre>
-                ${response.data}
-            </pre>
-        `);
+        $('#content').show();
     }).catch((error) => {
         $('#loading').hide();
         $('#content').html(`
@@ -27,3 +23,31 @@ $(document).ready(function () {
     })
 })
 
+function init () {
+    // console.log('initatied')
+    var id = getUrlParameter("id");
+    var token = getUrlParameter("token")
+    if(typeof id != "undefined" && typeof token != "undefined") {
+        $("#streamName").text(id);
+        document.title = `Futurelines || متابعة حصة ${id}`
+    }
+    else {
+        id = getUrlParameter("name");
+        if (typeof id != "undefined" && typeof token != "undefined") {
+            $("#streamName").text(id);
+            document.title = `Futurelines || متابعة حصة ${id}`
+        } 
+        else {
+            var current = window.location.pathname;
+            if (current == '/') {
+                window.location.replace("./unauthorised.html");
+            } else {
+                console.log('Not on the homepage.')
+            }
+        }
+    }
+}
+
+$(function() {
+    init();
+});
--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,17 @@ export async function ping() {
     })
     return data;
 }
+export function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
